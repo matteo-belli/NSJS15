f9c2d801f2af10e87f920bba7e31dbc560345208
Matteo Belli <matteobelli.design@gmail.com>
2023-08-28 14:19:59 +0200

    Flip sprite by direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //sprite a destra se vado a destra
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+
+        //sprite a sinistra se vado a sinistra
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
 
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
