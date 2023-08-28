672fac2663fb93d7609365fad9252dfb708c9b34
Matteo Belli <matteobelli.design@gmail.com>
2023-08-28 15:15:49 +0200

    Animator con animazioni (con sprite fittizie)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool Grounded;
 
     private void Awake()
     {
+        //prendo le ref al rigidbody e all'animator dal player
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -25,8 +29,32 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && Grounded)
+            Jump();
+
+
+        //setto i parametri dell'animator, se premo destra o sinistra parte l'animazione
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("Grounded", Grounded);
+
     }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("Jump");
+        Grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            Grounded = true;
+    }
+
+
+
+
+
 }
 
